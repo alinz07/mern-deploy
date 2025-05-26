dcf484eaa26a63d05eb828f3767ef701a9b8f690
Tony Linz <tonyllinz@gmail.com>
2025-05-25 18:24:00 -0700

    remove api base again

--- client/src/TodoItem.js
+++ client/src/TodoItem.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const API_BASE = process.env.API_BASE_TEST;
+const API_BASE = "";
 
 function TodoItem(props) {
 	const { name, id, completed, setItems } = props;
