91d76ffc24d361125cf3f15ebf197c7db693650f
Tony Linz <tonyllinz@gmail.com>
2025-07-13 09:34:02 -0700

    replace local host with backend url in login and reg components

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
 		e.preventDefault();
 		try {
 			const res = await axios.post(
-				"http://localhost:5000/api/auth/register",
+				"https://mern-deploy-1-hixt.onrender.com/api/auth/register",
 				{
 					username,
 					password,
