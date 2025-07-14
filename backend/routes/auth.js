5440d0588d783c2ca420503b6334022da8688b56
Tony Linz <tonyllinz@gmail.com>
2025-07-13 17:47:13 -0700

    add students to routhes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 const User = require("../models/User");
+const User = require("../models/Student");
 
 console.log("Auth routes loaded");
 
