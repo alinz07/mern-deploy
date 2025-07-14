353f5197f8e4ea4b3340a02ad7ec15c3e8977304
Tony Linz <tonyllinz@gmail.com>
2025-07-13 18:57:53 -0700

    routes/students.js update

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,17 +1,16 @@
 // server/routes/auth.js
 const express = require("express");
 const router = express.Router();
-const config = require("../config");
 const Student = require("../models/Student");
 
 // Get All Students
 router.get("/", async (req, res) => {
+	console.log("GET /api/students called");
 	try {
-		const students = await Student.find();
-
+		const students = await Student.find(); // Returns [] if empty
 		res.json(students);
 	} catch (err) {
-		console.error(err.message);
+		console.error("Error fetching students:", err.message);
 		res.status(500).send("Server Error");
 	}
 });
